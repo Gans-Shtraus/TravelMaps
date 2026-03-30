67299dffa255c32b5c24873c3a588152be360f93
Gans-Shtraus <pugachev.2013@yandex.ru>
2026-03-30 18:44:36 +0300

    fix

--- travel-app-backend/config/database.ts
+++ travel-app-backend/config/database.ts
@@ -1,7 +1,12 @@
-import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
 
-dotenv.config();
+require("dotenv").config();
+console.log("DB_DIALECT:", process.env.DB_DIALECT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 const {
   DB_HOST = "localhost",
